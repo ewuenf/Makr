bb96a205f94833fc9698c6dec20bd0b9b9bb0f4c
uenf-master <github@uenf.de>
2011-10-05 23:20:58 +0200

    changed license and some fixes in examples

--- examples/DynamicLib/Makrfile.rb
+++ examples/DynamicLib/Makrfile.rb
@@ -9,41 +9,48 @@ $localDir = File.dirname($arguments.scriptFile)
 $buildDir = $arguments.arguments[0]
 $target = $arguments.arguments[1]
 
+
+puts "target in DynamicLib: #{$target}"
+
+
+def buildAll()
+  # first load build caches etc.
+  build = Makr.loadBuild($buildDir)
+
+  # then use build block concept to ensure the build is saved after block has run through
+  build.saveAfterBlock do
+
+    # create some Config instances for use during build (they may already exist)
+    compilerConfig = build.makeNewConfig("CompileTask")
+    compilerConfig.clear()
+    compilerConfig["compiler"] = "g++"
+    compilerConfig["compiler.includePaths"] = " -I" + $localDir + "/src"
+    compilerConfig["compiler.cFlags"] = " -fPIC "
+
+
+    # then we collect all relevant files and apply generators to them
+    allCPPFiles = Makr::FileCollector.collect($localDir + "/src/", "*.{cpp,cxx}", true)
+    # compilerConfig could be specified directly here, but we use the name
+    # (as the configuration part of the script may be far away)
+    tasks = Makr.applyGenerators(allCPPFiles, [Makr::CompileTaskGenerator.new(build, build.getConfig("CompileTask"))])
+
+    # so this Makrfile.rb is going to build a dynamic lib
+    myDynamicLibTask = Makr.makeDynamicLib($buildDir + "/libtest.so.1.2.3", build, tasks, nil)
+
+    # finally, just build the whole thing (only building things, that have changed since last call)
+    # we could use "build.nrOfThreads = <number>" here to specify the number of threads to be used upon building
+    build.nrOfThreads = 3
+    build.build()
+  end
+end
+
+
+
 # implement a simple clean target
 if($target == "clean")
   system("rm -f " + $buildDir + "/*")
-  exit
-end
-
-# when we're not cleaning, we build:
-
-# first load build caches etc.
-build = Makr.loadBuild($buildDir)
-
-# then use build block concept to ensure the build is saved after block has run through
-build.saveAfterBlock do
-
-  # create some Config instances for use during build (they may already exist)
-  compilerConfig = build.makeNewConfig("CompileTask")
-  compilerConfig.clear()
-  compilerConfig["compiler"] = "g++"
-  compilerConfig["compiler.includePaths"] = " -I" + $localDir + "/src"
-  compilerConfig["compiler.cFlags"] = " -fPIC "
-
-
-  # then we collect all relevant files and apply generators to them
-  allCPPFiles = Makr::FileCollector.collect($localDir + "/src/", "*.{cpp,cxx}", true)
-  # compilerConfig could be specified directly here, but we use the name
-  # (as the configuration part of the script may be far away)
-  tasks = Makr.applyGenerators(allCPPFiles, [Makr::CompileTaskGenerator.new(build, build.getConfig("CompileTask"))])
-
-  # so this Makrfile.rb is going to build a dynamic lib
-  myDynamicLibTask = Makr.makeDynamicLib($buildDir + "/libtest.so.1.2.3", build, tasks, nil)
-
-  # finally, just build the whole thing (only building things, that have changed since last call)
-  # we could use "build.nrOfThreads = <number>" here to specify the number of threads to be used upon building
-  build.nrOfThreads = 3
-  build.build()
+else
+  buildAll()
 end
 
 
--- examples/makr_test/Makrfile.rb
+++ examples/makr_test/Makrfile.rb
@@ -13,60 +13,66 @@ $localDir = File.dirname($arguments.scriptFile)
 $buildDir = $arguments.arguments[0]
 $target = $arguments.arguments[1]
 
-# implement a simple clean target
-if($target == "clean") 
-  system("rm -f " + $buildDir + "/*")
-  exit
+
+puts "target in makr_test: #{$target}"
+
+
+def buildAll()
+
+  # first load build caches etc.
+  build = Makr.loadBuild($buildDir)
+
+  # then use build block concept to ensure the build is saved after block has run through
+  build.saveAfterBlock do
+
+    # create some Config instances for use during build (they may already exist), this example uses a qt3 setup
+    compilerConfig = build.makeNewConfig("CompileTask")
+    compilerConfig.clear()
+    compilerConfig["compiler"] = "g++"
+    compilerConfig["compiler.includePaths"] = " -I" + $localDir + "/src" + " -I/usr/lib/qt3/include"
+    compilerConfig["linker"] = "g++"
+    compilerConfig["linker.libs"] = " -lX11"
+    # use pkg-config to simplify config tasks
+    Makr::PkgConfig.addCFlags(compilerConfig, "libpng")
+    Makr::PkgConfig.addLibs(compilerConfig, "libpng")
+    Makr::PkgConfig.addCFlags(compilerConfig, "QtCore")
+    Makr::PkgConfig.addLibs(compilerConfig, "QtCore")
+
+
+    # then we collect all relevant files and apply generators to them
+    allCPPFiles = Makr::FileCollector.collect($localDir + "/src/", "*.{cpp,cxx}", true)
+    # compilerConfig could be specified directly here, but we use the name
+    # (as the configuration part of the script may be far away)
+    tasks = Makr.applyGenerators(allCPPFiles, [Makr::CompileTaskGenerator.new(build, build.getConfig("CompileTask"))])
+    allHeaderFiles = Makr::FileCollector.collect($localDir + "/src/", "*.{h}", true)
+    tasks.concat(
+      Makr.applyGenerators(allHeaderFiles, [Makr::MocTaskGenerator.new(build, build.getConfig("CompileTask"))])
+                )
+
+    # so this Makrfile.rb is going to build a program
+    myProgramTask = Makr.makeProgram($buildDir + "/myProgram", build, tasks, build.getConfig("CompileTask"))
+
+    #myProgramTask.printDependencies()
+    #exit 0
+
+    # set special options for a single task
+    compileTaskName = Makr::CompileTask.makeName($localDir + "/src/A.cpp")
+    if(build.hasTask?(compileTaskName)) then
+      task = build.getTask(compileTaskName)
+      specialConf = build.makeNewConfigForTask(compileTaskName + "_Conf", task)
+      specialConf.copyAddUnique("compiler.includePaths", " -I/usr/include")
+    end
+
+    # finally, just build the whole thing (only building things, that have changed since last call)
+    # we could use "build.nrOfThreads = <number>" here to specify the number of threads to be used upon building
+    #build.nrOfThreads = 1 # a single thread can be helpful for debugging
+    build.build()
+  end
 end
 
-# when we're not cleaning, we build:
-
-# first load build caches etc.
-build = Makr.loadBuild($buildDir)
-
-# then use build block concept to ensure the build is saved after block has run through
-build.saveAfterBlock do
-
-  # create some Config instances for use during build (they may already exist), this example uses a qt3 setup
-  compilerConfig = build.makeNewConfig("CompileTask")
-  compilerConfig.clear()
-  compilerConfig["compiler"] = "g++"
-  compilerConfig["compiler.includePaths"] = " -I" + $localDir + "/src" + " -I/usr/lib/qt3/include"
-  compilerConfig["linker"] = "g++"
-  compilerConfig["linker.libs"] = " -lX11"
-  # use pkg-config to simplify config tasks
-  Makr::PkgConfig.addCFlags(compilerConfig, "libpng")
-  Makr::PkgConfig.addLibs(compilerConfig, "libpng")
-  Makr::PkgConfig.addCFlags(compilerConfig, "QtCore")
-  Makr::PkgConfig.addLibs(compilerConfig, "QtCore")
-
-
-  # then we collect all relevant files and apply generators to them
-  allCPPFiles = Makr::FileCollector.collect($localDir + "/src/", "*.{cpp,cxx}", true)
-  # compilerConfig could be specified directly here, but we use the name
-  # (as the configuration part of the script may be far away)
-  tasks = Makr.applyGenerators(allCPPFiles, [Makr::CompileTaskGenerator.new(build, build.getConfig("CompileTask"))])
-  allHeaderFiles = Makr::FileCollector.collect($localDir + "/src/", "*.{h}", true)
-  tasks.concat(
-    Makr.applyGenerators(allHeaderFiles, [Makr::MocTaskGenerator.new(build, build.getConfig("CompileTask"))])
-              )
-
-  # so this Makrfile.rb is going to build a program
-  myProgramTask = Makr.makeProgram($buildDir + "/myProgram", build, tasks, build.getConfig("CompileTask"))
-
-  #myProgramTask.printDependencies()
-  #exit 0
-
-  # set special options for a single task
-  compileTaskName = Makr::CompileTask.makeName($localDir + "/src/A.cpp")
-  if(build.hasTask?(compileTaskName)) then
-    task = build.getTask(compileTaskName)
-    specialConf = build.makeNewConfigForTask(compileTaskName + "_Conf", task)
-    specialConf.copyAddUnique("compiler.includePaths", " -I/usr/include")
-  end
-
-  # finally, just build the whole thing (only building things, that have changed since last call)
-  # we could use "build.nrOfThreads = <number>" here to specify the number of threads to be used upon building
-  #build.nrOfThreads = 1 # a single thread can be helpful for debugging
-  build.build()
+# implement a simple clean target
+if($target == "clean")
+  system("rm -f " + $buildDir + "/*")
+else
+  buildAll()
 end
