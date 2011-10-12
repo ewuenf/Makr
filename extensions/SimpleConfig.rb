ecae1f1a2a9222ef04b0bf2ea6696f475643806f
uenf-master <github@uenf.de>
2011-10-13 00:04:36 +0200

    working on a new extension

---
+++ extensions/SimpleConfig.rb
@@ -0,0 +1,19 @@
+
+module Makr
+
+
+
+  # Convenience class related to Config creation providing only file names
+  class SimpleConfig
+
+    attr_accessor :libs, :libPaths
+
+    def initialize()
+    end
+
+  end
+
+
+
+end
+
